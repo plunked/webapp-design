a08401c229b851d25467978dd8157b13a9ed75d1
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2015-10-20 21:34:59 +0800

    large update - moved folders around and created caseStudy04

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Web Application Design Module
 
 This repo contains the weekly workload as well as the final project for EE4717 Web Application Design from Nanyan Technological University. Once the module has been completed, I will clean up the repo so that future me as well as other students can benefit. 
+
+Current Status:
+
+HTML/CSS
+Week 01
+Week 02
+Week 03
+caseStudy02
+
+Week 06 - Javascript
+Week 07 - PHP
+caseStudy03 - Javascript and PHP integration
+Week 08 - SQL
+caseStudy04 - PHP and SQL integration
+Week 09 - Session Handling
+
+how's catering - final project
---
+++ caseStudy04/jobApplication.php
@@ -0,0 +1,26 @@
+<?php 
+$applicantName = $_POST['applicantName'];
+$applicantEmail = $_POST['applicantEmail'];
+$applicantStartDate = $_POST['applicantStartDate'];
+$applicantExperience = $_POST['$applicantExperience'];
+
+if(!get_magic_quotes_gpc()){
+	$applicantName = addslashes($applicantName);
+	$applicantExperience = addslashes($applicantExperience);
+}
+
+@ $db = new mysqli('localhost', 'root', 'password', 'caseStudy');
+
+if(mysqli_connect_errno()){
+	echo "Error: Could not connect to database. Please check if VM is running.";
+	exit;
+}
+
+$query = "INSERT into jobApplicants values
+			('".$applicantEmail."', '".$applicantName."', '".$applicantStartDate."', '".$applicantEmail."')";
+			
+$result = $db -> query($query);
+
+$db-> close();
+header('Location:/done.php'); //modify the location to an absolute URI for production code
+?>

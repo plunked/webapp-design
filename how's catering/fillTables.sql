2f87379dc823d691d1882f3415593e3f334a2ddd
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2015-10-24 18:43:16 +0800

    commited tables for db

---
+++ how's catering/fillTables.sql
@@ -0,0 +1,14 @@
+use f33ee;
+
+insert into packages values
+(NULL, "Basic Package",50, "1,2,3", "1,2,3", "1,2,3", "1,2,3"),
+(NULL, "Intermediate Package",75, "1,2,3", "1,2,3", "1,2,3", "1,2,3"),
+(NULL, "Advanced Package",100, "1,2,3", "1,2,3", "1,2,3", "1,2,3");
+
+insert into dishes values
+(null, "meat", "Chicken 65", "Awesome chicken", "/img/dishes/chicken65"),
+(null, "meat", "Mutton Mysore", "Awesome mutton", "/img/dishes/mysoreMutton"),
+(null, "meat", "Fish Tikka", "Awesome fish", "/img/dishes/fishTikka");
+
+insert into transactions values
+(null, "Rubern", 81830747, "m.rubern.c@gmail.com", "APT BLK 290B BUKIT BATOK ST24 #17-75 S653290", "1100", "1700", "Paypal", 50);

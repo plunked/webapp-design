090829abf166fa9a5e278fb23713e9f0911f9291
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2015-10-25 11:23:53 +0800

    test selector

--- how's catering/packageSelector.php
+++ how's catering/packageSelector.php
@@ -35,21 +35,40 @@
 			<article>
 				<h3>Package Selector</h3>
 				<?php 
-				$selectedPackage = $_POST['packageChoice']
-				@ $db = new mysqli('localhost', 'f33ee', 'f33ee', 'f33ee');
-
-				if (mysqli_connect_errno()) {
-					echo 'Error: Could not connect to database.  Please try again later.';
-					exit;
-				}
-				
-			$query = "select * from packages where package_cost like".$selectedPackage;
-			$result = $db ->query($query);
-			$num_results = $result->num_rows;
-			
-			
-
-				
+					$selectedPackage = $_POST['packageChoice']
+					@ $db = new mysqli('localhost', 'f33ee', 'f33ee', 'f33ee');
+	
+					if (mysqli_connect_errno()) {
+						echo 'Error: Could not connect to database.  Please try again later.';
+						exit;
+					}
+					
+					$query = "select * from packages where package_cost like".$selectedPackage;
+					$result = $db ->query($query);
+					$row = $result->fetch_assoc();
+					
+					$packageID = $row['packageid'];
+					$packageName = $row['packagename'];
+					$packageCost = $row['package_cost'];
+					$mainChoice1 = $row['main_1_choices'];
+					$mainChoice2 = $row['main_2_choices'];
+					$optChoice1 = $row['opt_1_choices'];
+					$optChoice2 = $row['opt_2_choices'];
+					
+					$mainChoice1Arr = explode(",", $mainChoice1);
+					$mainChoice2Arr = explode(",", $mainChoice2);
+					echo "<form method='dishSelection.php' action='POST'>";
+					echo "<select id= 'main1Selection'";
+					echo '<option value="'.$mainChoice1Arr[0].'">'.$mainChoice1Arr[0].'</option>';
+					echo '<option value="'.$mainChoice1Arr[1].'">'.$mainChoice1Arr[1].'</option>';
+					echo '<option value="'.$mainChoice1Arr[2].'">'.$mainChoice1Arr[2].'</option>';
+					
+					echo "<select id='main2Selection'";
+					echo '<option value="'.$mainChoice2Arr[0].'">'.$mainChoice2Arr[0].'</option>';
+					echo '<option value="'.$mainChoice2Arr[1].'">'.$mainChoice2Arr[1].'</option>';
+					echo '<option value="'.$mainChoice2Arr[2].'">'.$mainChoice2Arr[2].'</option>';
+					
+					echo "</form>";
 				?>
 			</article>
 		</section>
